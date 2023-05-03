f68b3018f086bf5cfe2bd59dd69893f6137b0228
ChangJun <projectchangjun@naver.com>
2023-05-03 12:45:57 +0900

    22

--- src/main/java/com/groupc/weather/WeatherApplication.java
+++ src/main/java/com/groupc/weather/WeatherApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WeatherApplication {
 
+	// 주석
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherApplication.class, args);
 	}
