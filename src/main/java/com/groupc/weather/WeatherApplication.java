bfbe5f6a0e9bbcb143fc34b0cb409bed405c15dd
yongyong216 <godqhr6085@naver.com>
2023-05-08 13:36:59 +0900

    2023.05.08.

--- src/main/java/com/groupc/weather/WeatherApplication.java
+++ src/main/java/com/groupc/weather/WeatherApplication.java
@@ -9,12 +9,9 @@ public class WeatherApplication {
 	// 주석
 	// 주석 1
 	// 주석2 asdasd
-<<<<<<< HEAD
 	// c
-=======
 	// fjasdhfkgit
 
->>>>>>> 9250e858ee635d2f019a935794bcf6bd9dc25caa
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherApplication.class, args);
 	}
