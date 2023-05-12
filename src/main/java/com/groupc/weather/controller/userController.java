01622aa192ef78725e27a570a91ec3f8089699e3
jeoungBin <eoehdektnv78@naver.com>
2023-05-12 11:34:13 +0900

    05-12 11:34

--- src/main/java/com/groupc/weather/controller/userController.java
+++ src/main/java/com/groupc/weather/controller/userController.java
@@ -4,14 +4,16 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.groupc.weather.dto.ResponseDto;
+import com.groupc.weather.dto.request.user.LoginUserRequestDto;
 import com.groupc.weather.dto.request.user.PostUserRequestDto;
-import com.groupc.weather.service.PhotoBoardService;
+import com.groupc.weather.dto.response.LoginUserResponseDto;
 import com.groupc.weather.service.UserService;
 
 @RestController
@@ -25,10 +27,27 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping("")
+    // 유저 등록
+    @PostMapping("sign-up")
     public ResponseEntity<ResponseDto> postUser(
             @Valid @RequestBody PostUserRequestDto requestBody) {
         ResponseEntity<ResponseDto> reposne = userService.postUser(requestBody);
         return reposne;
     }
+
+    // 유저 로그인
+    @PostMapping("sign-in")
+    public ResponseEntity<? super LoginUserResponseDto> LoginUser(
+            @Valid @RequestBody LoginUserRequestDto requestBody) {
+        ResponseEntity<? super LoginUserResponseDto> reponse = userService.LoginUser(requestBody);
+        return reponse;
+    }
+
+    // 유저 이메일 찾기
+    @PostMapping("find-email")
+    
+    // 유저 비밀번호 찾기
+    // 유저 정보 수정
+    // 유저 정보 삭제
+
 }
